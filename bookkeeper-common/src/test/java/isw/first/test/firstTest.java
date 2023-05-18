e6699e0c3369e0f544ae15cdf555fd062c647ab2
Luca Saverio Esposito <esposito.1917959@studenti.uniroma1.it>
2023-05-18 12:36:35 +0200

    First commit, removed test classes and added a dummy test on module bookeper-common

---
+++ bookkeeper-common/src/test/java/isw/first/test/firstTest.java
@@ -0,0 +1,11 @@
+package isw.first.test;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class firstTest {
+    @Test
+    public void testOne(){
+        Assert.assertTrue(true);
+    }
+}
